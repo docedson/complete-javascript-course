4fa01711ea259b21cca908f0c6c936df5f4bf692
ae074267 <C_Edson.Andrew@cerner.com>
2020-01-30 12:52:28 -0600

    Finished Section 3 videos

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,19 +1,32 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// // functions
+// calculateAge(1965);
+
+// function calculateAge(year) {
+//   console.log(2016 - year);
+// }
 
 
+// // retirement(1956);
+
+// var retirement = function(year) {
+//   console.log(65 - (2016 - year));
+// }
 
 
+// // variables
+// console.log(age);
+// var age = 23
 
-
-
-
-
-
-
-
-
+// function foo() {
+//   console.log(age);
+//   var age = 65;
+//   console.log(age);
+// }
+// foo();
+// console.log(age)
 
 
 
@@ -21,26 +34,28 @@
 // Lecture: Scoping
 
 
-// First scoping example
+// // First scoping example
 
-/*
-var a = 'Hello!';
-first();
+// var a = 'Hello!';
+// first();
 
-function first() {
-    var b = 'Hi!';
-    second();
+// function first() {
+//     var b = 'Hi!';
+//     second();
 
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
-}
-*/
+//     function second() {
+//         var c = 'Hey!';
+//         third()
+//     }
+// }
 
+// function third() {
+//   var d = 'John';
+//   // console.log(c);
+//   console.log(a+d);
+// }
 
-
-// Example to show the differece between execution stack and scope chain
+// Example to show the difference between execution stack and scope chain
 
 /*
 var a = 'Hello!';
@@ -67,9 +82,41 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this);
+
+// calculateAge(1985);
+
+// function calculateAge(year) {
+//   console.log(2016 - year);
+//   console.log(this);
+// }
 
 
+var john = {
+  name: 'John',
+  yearOfBirth: 1990,
+  calculateAge: function() {
+    console.log(this);
+    console.log(2016 - this.yearOfBirth);
+    /*
 
+    function innerFunction() {
+      console.log(this);
+    }
+    innerFunction();
+    */
+  }
+}
+
+john.calculateAge();
+
+var mike = {
+  name: 'Mike',
+  yearOfBirth: 1984
+};
+
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
 
 
 
